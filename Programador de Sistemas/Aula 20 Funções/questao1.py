a132f135bc7b386da9fde0f81b110cdf3a2132de
João Paulo <jpsampaiosenac@gmail.com>
2026-07-09 11:25:44 -0300

    Att atividades'funções'

--- Programador de Sistemas/Aula 20 Funções/questao1.py
+++ Programador de Sistemas/Aula 20 Funções/questao1.py
@@ -3,22 +3,21 @@ desses três argumentos."""
 def argumentos():
     while True:
         print("""Escolha uma opçao
-            1 - soma
-            2 - Multiplicação
-            0 - sair""")
+            1 - Soma
+            2 - Sair""")
         opcao = input("Qual você quer?")
 
         if opcao == "1":
             n1, n2, n3 = numeros()
             soma(n1, n2, n3)
-        elif opcao == "0":
+        elif opcao == "2":
             print("Saindo do programa...")
             break
 
 
 def soma(a, b, c):
     resultado = a + b + c
-    print(f"O resultado da soma é {soma}.")
+    print(f"O resultado da soma é {resultado }.")
     
 def numeros():
     n1 = int(input("Numero: "))
