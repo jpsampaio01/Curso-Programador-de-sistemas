db7d5773fbbd0d12eb8d808f0d40a6e55e375892
João Paulo <jpsampaiolv@gmail.com>
2026-08-04 11:22:29 -0300

    Atualização

--- Programador de Sistemas/Aula 28 GUI/multiplastelas.py
+++ Programador de Sistemas/Aula 28 GUI/multiplastelas.py
@@ -1,15 +1,30 @@
 import tkinter as tk
 
-root = tk.Tk
-root.title("Interface com Frame")
 
-frame = tk.Frame(root, borderwidth=2, relief="sunken")
-frame.pack(padx=10, pady=10)
+def mudar():
+    if tela1.winfo_viewable():
+        tela1.pack_forget()
+        tela2.pack(fill="both", expand=True)
+    else:
+        tela2.pack_forget()
+        tela1.pack(fill="both", expand=True)
 
-label = tk.Label(frame, text="Este é um frame!")
-label.pack(padx=5, pady=5)
 
-button = tk.Button(frame, text="Clique aqui")
-button.pack(padx=5, pady=5)
+janela = tk.Tk()
+janela.geometry("800x700")
+janela.resizable(False, False)
 
-root.mainloop()
+tela1 = tk.Frame(janela,  bg="lightgreen")
+label1 = tk.Label(tela1, text="Tela 1")
+label1.pack(pady=10)
+botao1 = tk.Button(tela1, text="Mudar", command=mudar)
+botao1.pack(pady=10)
+
+tela2 = tk.Frame(janela, bg="pink")
+label2 = tk.Label(tela2, text="Tela 2")
+label2.pack(pady=10)
+botao2 = tk.Button(tela2, text="Mudar", command=mudar)
+botao2.pack(pady=10)
+
+tela1.pack(fill="both", expand=True)
+janela.mainloop()
