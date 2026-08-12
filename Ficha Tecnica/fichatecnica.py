1f5643966ebd24f67ea953c5349a98bae7a66b45
João Paulo <jpsampaiolv@gmail.com>
2026-08-12 11:14:52 -0300

    Atualização

--- Ficha Tecnica/fichatecnica.py
+++ Ficha Tecnica/fichatecnica.py
@@ -1,92 +1,102 @@
-# import tkinter as tk
-# import tkinter.messagebox as messagebox
-# import sqlite3
+import tkinter as tk
+import tkinter.messagebox as messagebox
+import sqlite3
 
-# # Conexão com o banco de dados
-# conexao = sqlite3.connect('ficha_tecnica.db')
-# cursor = conexao.cursor()
+# Conexão com o banco de dados
+conexao = sqlite3.connect('ficha_tecnica.db')
+cursor = conexao.cursor()
 
-# # Tabela da ficha técnica
-# cursor.execute('''CREATE TABLE IF NOT EXISTS Ficha_Tecnica(
-#         ID INTEGER PRIMARY KEY,
-#         Ingredientes TEXT NOT NULL,
-#         Quantidade_Comprada INTEGER,
-#         Preco_Comprado REAL,
-#         Quantidade_Usada INTEGER,
-#         Unidade TEXT,
-#         Preco_gasto REAL)
-#         ''')
+#Cores Padrao
+COR_FUNDO = "#fef3e8"
+COR_TEXTO = "#000000"
+COR_BOTAO = "#743808"
+COR_BOTAO_TEXTO = "#F7F2F2"
+COR_BOTAO_2 = "#e9dcce"
 
-# # Tabela ingredientes
-# cursor.execute('''CREATE TABLE IF NOT EXISTS ingredientes(
-#         ID INTEGER PRIMARY KEY,
-#         nome_ingrediente TEXT NOT NULL)
-#         ''')
+# criando janela
+janela = tk.Tk()
+janela.title("Ficha Técnica")
+janela.geometry("800x800+100+100")
+janela.configure(bg=COR_FUNDO)
+janela.resizable(False, False)
 
-# # Tabela produtos
-# cursor.execute('''CREATE TABLE IF NOT EXISTS produtos(
-#         ID INTEGER PRIMARY KEY,
-#         nome_produto TEXT NOT NULL)
-#         ''')
+janela2 = tk.Tk()
+janela2.title("Ficha Técnica")
+janela2.geometry("800x800+100+100")
+janela2.configure(bg=COR_FUNDO)
+janela2.resizable(False, False)
 
-# conexao.commit()
-
-# # Variáveis globais para texto nas telas
-# entry_usuario = None
-# entry_senha = None
-# entry_usuario_cadastro = None
-# entry_senha_cadastro = None
-# entry_senha_confirmar = None
-
-# #Cores Padrao
-# COR_FUNDO = "#fef3e8"
-# COR_TEXTO = "#000000"
-# COR_BOTAO = "#743808"
-# COR_BOTAO_TEXTO = "#F7F2F2"
-# COR_BOTAO_2 = "#e9dcce"
-
-# def mostrar_menu():
-#     pass
-
-# def limpar_tabela():
-#     pass
-
-# def buscar_dados():
-#     pass
+# criando frame
+frame = tk.Frame(janela)
+# label = tk.Label(janela, text="Procurar Por",   bg=COR_FUNDO)
+# label.pack(padx=20, pady=5)
 
 
-# # Inserir os dados na tabela de igredientes
+label1 = tk.Label(janela, text="Ficha Técnica de Preparo", bg= COR_FUNDO)
+label1.pack()
+label1.pack(padx= 100, pady= 5)
 
-# def cadastrar_ing():
-#     pass
+label2 = tk.Label(janela2, text="Ficha Técnica de Preparo", bg= COR_FUNDO)
+label2.pack()
+label2.pack(padx= 100, pady= 5)
 
-# def listar_ing():
-#     pass
 
-# def buscar_ing():
-#     pass
+janela.mainloop()
 
-# def atualizar_ing():
-#     pass
+# Tabela da ficha técnica
+cursor.execute('''CREATE TABLE IF NOT EXISTS Ficha_Tecnica(
+        ID INTEGER PRIMARY KEY,
+        Ingredientes TEXT NOT NULL,
+        Qunt_Comprada INTEGER,
+        Valor_Comprado REAL,
+        Qunt_Usada INTEGER,
+        Unid_Medida TEXT,
+        Valor_Usado REAL)
+        ''')
 
-# def excluir_ing():
-#     pass
+# Tabela do modo de preparo
+cursor.execute('''CREATE TABLE IF NOT EXISTS Preparo(
+        ID INTEGER PRIMARY KEY,
+        ingrediente TEXT NOT NULL,
+        modo_de_preparo TEXT NOT NULL)
+        ''')
 
-# # ____________________________ INTERFACE ________________________ #
+conexao.commit()
 
-# def tela_ingredientes():
+def mostrar_menu():
+    pass
 
-#     # criando janela
-#     janela = tk.Tk()
-#     janela.title("Ficha Técnica")
-#     janela.geometry("900x900")
-#     janela.resizable(False, False)
+def limpar_tabela():
+    pass
 
-#     # criando frame
-#     frame = tk.Frame(janela)
+def buscar_dados():
+    pass
 
-#     # label 1
-#     label1 = tk.Label(janela, text="Ficha Técnica")
-#     label1.pack()
 
-#     janela.mainloop()
+# Inserir os dados na tabela de igredientes
+
+def cadastrar_ing():
+    print("Cadastrar Ingrediente")
+    nome_ingrediente = campo_nome.get()
+    quantidade_comprada = campo_qunt.get()
+    valor_comprado = campo_valor_comp.get()
+    quant_usada = campo_qunt_usada.get()
+    unidade = campo_unidade.get()
+    valor_usado = campo_valor_usado.get()
+
+def listar_ing():
+    pass
+
+def buscar_ing():
+    pass
+
+def atualizar_ing():
+    pass
+
+def excluir_ing():
+    pass
+
+# ____________________________ INTERFACE ________________________ #
+
+def tela_ingredientes():
+    pass
